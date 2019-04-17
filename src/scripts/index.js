e8491907ab40ef0fd08ff98706229f25e8072fe4
Mark Hoad <markdhoad@gmail.com>
2019-04-17 16:16:26 +1000

    Added show more locations feature. Closes #5

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,8 @@ import '../styles/index.scss';
 import '../../public/beer-1.jpg';
 
 console.log('webpack starterkit');
+
+document.querySelector('.locations__btn').addEventListener('click', function(){
+  document.querySelector('.is-hidden').classList.remove('is-hidden');
+  document.querySelector('.locations__btn').classList.add('is-hidden');
+});
