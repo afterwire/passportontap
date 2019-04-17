c1d4dc1be1921aa429a29d8d7eed3272fcef64be
Mark Hoad <markdhoad@gmail.com>
2019-04-17 20:43:57 +1000

    Add live map. Closes #6

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,3 +7,113 @@ document.querySelector('.locations__btn').addEventListener('click', function(){
   document.querySelector('.is-hidden').classList.remove('is-hidden');
   document.querySelector('.locations__btn').classList.add('is-hidden');
 });
+
+// MAPS CODE
+// Style credit: https://snazzymaps.com/style/1/pale-dawn
+const mapStyle = [
+  {
+    "featureType": "administrative",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "lightness": 33
+      }
+    ]
+  },
+  {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [
+      {
+        "color": "#f2e5d4"
+      }
+    ]
+  },
+];
+
+// Escapes HTML characters in a template literal string, to prevent XSS.
+// See https://www.owasp.org/index.php/XSS_%28Cross_Site_Scripting%29_Prevention_Cheat_Sheet#RULE_.231_-_HTML_Escape_Before_Inserting_Untrusted_Data_into_HTML_Element_Content
+function sanitizeHTML(strings) {
+  const entities = {'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;'};
+  let result = strings[0];
+  for (let i = 1; i < arguments.length; i++) {
+    result += String(arguments[i]).replace(/[&<>'"]/g, (char) => {
+      return entities[char];
+    });
+    result += strings[i];
+  }
+  return result;
+}
+
+function initMap() {
+
+  // Create the map.
+  const map = new google.maps.Map(document.getElementById('google-map'), {
+    zoom: 6,
+    center: {lat: 52.632469, lng: -1.689423},
+    styles: mapStyle
+  });
+
+  // Load the stores GeoJSON onto the map.
+  map.data.loadGeoJson('../../public/stores.json');
+
+  // Define the custom marker icons, using the store's "category".
+  map.data.setStyle(feature => {
+    return {
+      icon: {
+        url: `../../public/icon_${feature.getProperty('category')}.png`,
+        scaledSize: new google.maps.Size(64, 64)
+      }
+    };
+  });
+
+  const apiKey = 'AIzaSyDIPIijEgq2NsXQWYwnK6Ziio7C4ifzkMw';
+  const infoWindow = new google.maps.InfoWindow();
+  infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
+
+  // Show the information for a store when its marker is clicked.
+  map.data.addListener('click', event => {
+
+    const category = event.feature.getProperty('category');
+    const name = event.feature.getProperty('name');
+    const description = event.feature.getProperty('description');
+    const hours = event.feature.getProperty('hours');
+    const phone = event.feature.getProperty('phone');
+    const position = event.feature.getGeometry().get();
+    const content = sanitizeHTML`
+      <img style="float:left; width:200px; margin-top:30px" src="img/logo_${category}.png">
+      <div style="margin-left:220px; margin-bottom:20px;">
+        <h2>${name}</h2><p>${description}</p>
+        <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
+        <p><img src="https://maps.googleapis.com/maps/api/streetview?size=350x120&location=${position.lat()},${position.lng()}&key=${apiKey}"></p>
+      </div>
+    `;
+
+    infoWindow.setContent(content);
+    infoWindow.setPosition(position);
+    infoWindow.open(map);
+  });
+
+}
+
+const loadGoogleMaps = (callback) => {
+  const existingScript = document.getElementById('googlemaps');
+
+  if (!existingScript) {
+    const script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDIPIijEgq2NsXQWYwnK6Ziio7C4ifzkMw&libraries=places';
+    script.id = 'googlemaps';
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      if (callback) callback();
+    };
+  }
+
+  if (existingScript && callback) callback();
+};
+
+loadGoogleMaps(initMap);
